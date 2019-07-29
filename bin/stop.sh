e974ed2044e54d9dae765aa612a83d0242488f08
claudiols1979 <claudiols1979@hotmail.com>
2019-07-29 17:25:11 +0000

    add sudo capability to rm in stop.sh

--- bin/stop.sh
+++ bin/stop.sh
@@ -4,6 +4,6 @@ if [ -d /var/www/timeoff-management ]
 then
     cd /var/www/timeoff-management/bin/
     pm2 stop 0
-    rm -rf /var/www/timeoff-management
+    sudo rm -rf /var/www/timeoff-management
 fi
 
