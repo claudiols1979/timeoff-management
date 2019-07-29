07a7c0ef1fc60a5b0275f8e23d81bde4342b53ac
claudiols1979 <claudiols1979@hotmail.com>
2019-07-29 17:52:43 +0000

    beforeinstall sudo

--- bin/beforeinstall.sh
+++ bin/beforeinstall.sh
@@ -1,46 +1,46 @@
 #!/bin/bash
 # OS UPDATE
-apt-get update
-apt-get install -y build-essential openssl libssl-dev pkg-config
+sudo apt-get update
+sudo apt-get install -y build-essential openssl libssl-dev pkg-config
 
 #NODEJS SETUP && SQLITE
-apt-get install -y build-essential openssl libssl-dev pkg-config
-apt-get install -y nodejs
-apt-get install npm -y
-npm cache clean -f
-npm install -g n
-apt-get install nginx git -y
-apt-get install sqlite -y
+sudo apt-get install -y build-essential openssl libssl-dev pkg-config
+sudo apt-get install -y nodejs
+sudo apt-get install npm -y
+sudo npm cache clean -f
+sudo npm install -g n
+sudo apt-get install nginx git -y
+sudo apt-get install sqlite -y
 
 #CLONE REPO - PUBLIC REPO
 cd /var/www
-git clone https://github.com/claudiols1979/timeoff-management.git
+sudo git clone https://github.com/claudiols1979/timeoff-management.git
 
 #SETUP NGINX && SQLITE
-apt-get install -y build-essential openssl libssl-dev pkg-config
+sudo apt-get install -y build-essential openssl libssl-dev pkg-config
 cd /etc/nginx/sites-available
-cp /var/www/timeoff-management/templates/timeoff-management /etc/nginx/sites-available/timeoff-management
-rm default
-systemctl start sqlite
+sudo cp /var/www/timeoff-management/templates/timeoff-management /etc/nginx/sites-available/timeoff-management
+sudo rm default
+sudo systemctl start sqlite
 
 #CREATE SYMBOLIC LINK
-ln -s /etc/nginx/sites-available/timeoff-management /etc/nginx/sites-enabled/timeoff-management
+sudo ln -s /etc/nginx/sites-available/timeoff-management /etc/nginx/sites-enabled/timeoff-management
 
 #REMOVE DEFAULT FROM SITES-ENABLED DIR
-rm /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
 
 #INSTALLING PM2 AND UPDATING PROJECT DEPENDENCIES
-npm install pm2 -g
+sudo npm install pm2 -g
 cd /var/www/
-chown -R ubuntu timeoff-management
+sudo chown -R ubuntu timeoff-management
 cd timeoff-management
-npm install
-npm audit fix --force
+sudo npm install
+sudo npm audit fix --force
 
 #Express public dir 
 cd public
-npm install
-npm audit fix
+sudo npm install
+sudo npm audit fix
 
 
 
